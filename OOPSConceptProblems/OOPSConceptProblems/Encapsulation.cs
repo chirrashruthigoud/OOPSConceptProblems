d265aac74e71acb9ce9d46c86ca341de6ada555d
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-04-28 20:29:31 +0530

    Add : Encapsulation in oops

---
+++ OOPSConceptProblems/OOPSConceptProblems/Encapsulation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConceptProblems
+{
+    public class Encapsulation
+    {
+        private int AccountNumber;
+
+       // private double Balance;
+      /*  public int SetAccountNum
+        {
+            get
+            {
+                return AccountNumber;
+            }
+            set
+            {
+                AccountNumber = value;
+            }
+        }
+           public int getAccountNum
+             {
+                 get; set;
+             }*/
+
+        public int getAccount()
+        {
+            return AccountNumber;
+        }
+        public void setAccount(int number)
+        {
+            AccountNumber = number;
+        }
+        
+    }
+}
--- OOPSConceptProblems/OOPSConceptProblems/Program.cs
+++ OOPSConceptProblems/OOPSConceptProblems/Program.cs
@@ -1,4 +1,5 @@
 ﻿using OOPSConceptProblems;
+using System.Security.Principal;
 
 namespace OOPSConceptProblem
 {
@@ -22,43 +23,45 @@ namespace OOPSConceptProblem
              name_one.cost = 67000;
              name_one.classandObject();*/
 
-            Student obj =new Student();
-            obj.Id = 1;
-            obj.Name = "sai";
-            obj.Address = "Hyd";
-            obj.Phone = "9876545785";
-            obj.Method();
-             obj.Class = 1;
-            obj.Grade = 'A';
-            obj.Marks = 554;
-             obj.Fees = 22000;
-            obj.ClassMethod();
-            Console.ReadLine();
+            /* Student obj =new Student();
+             obj.Id = 1;
+             obj.Name = "sai";
+             obj.Address = "Hyd";
+             obj.Phone = "9876545785";
+             obj.Method();
+              obj.Class = 1;
+             obj.Grade = 'A';
+             obj.Marks = 554;
+              obj.Fees = 22000;
+             obj.ClassMethod();
+             Console.ReadLine();
 
-            Staff obj_one=new Staff();
-            obj_one.Salary = 23000;
-            obj_one.Designation = "MathTeacher";
-            obj_one.StaffMethod();
+             Staff obj_one=new Staff();
+             obj_one.Salary = 23000;
+             obj_one.Designation = "MathTeacher";
+             obj_one.StaffMethod();
 
-            Teaching name = new Teaching();
-            name.Id = 2;
-            name.Name = "pari";
-            name.Address = "AP";
-            name.Phone = "9865456658";
-            name.Method();
-            name.Salary = 12000;
-            name.Designation = "MT";
-            name.StaffMethod();
-            name.Subject = "Maths";
-            name.Qualification = "Ms";
-            name.TeachingMethod();
+             Teaching name = new Teaching();
+             name.Id = 2;
+             name.Name = "pari";
+             name.Address = "AP";
+             name.Phone = "9865456658";
+             name.Method();
+             name.Salary = 12000;
+             name.Designation = "MT";
+             name.StaffMethod();
+             name.Subject = "Maths";
+             name.Qualification = "Ms";
+             name.TeachingMethod();*/
 
+            //Encapsulation
 
+            Encapsulation num = new Encapsulation();
+            /* num.SetAccountNum =456855276;
+             Console.WriteLine(num.SetAccountNum);*/
 
-
-           /* ChildClass obj_one=new ChildClass();
-            obj_one.ChildMethod();
-           // obj_one.ParentMethod();*/
+            num.setAccount(579876687);
+            Console.WriteLine("Account number : " + num.getAccount());
 
         }
     }
