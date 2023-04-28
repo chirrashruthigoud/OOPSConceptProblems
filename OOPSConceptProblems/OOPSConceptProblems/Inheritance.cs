dbc8e56172c7d51c465e088248587a22f3d61cac
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-04-28 19:21:01 +0530

    Add : Inheritance in oops

---
+++ OOPSConceptProblems/OOPSConceptProblems/Inheritance.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConceptProblems
+{
+    public class Inheritance
+    {
+        public int Id;
+        public string Name,Address, Phone;
+        public void Method() 
+        {
+            // Console.WriteLine(Id + " "  + Name + " " + Address + " " + Phone);
+            Console.WriteLine("Id number:" + Id);
+            Console.WriteLine("Name :" + Name);
+            Console.WriteLine("Address:" + Address);
+            Console.WriteLine("Phone No :" + Phone);
+        }
+    }
+    public class Student:Inheritance
+    {
+        public int Class;
+        public char Grade;
+       public float Marks, Fees;
+
+        public void ClassMethod()
+        {
+           // Console.WriteLine(Class + " " + Grade + " " +Marks + " " + Fees);
+            Console.WriteLine("Class Name :" + Class);
+            Console.WriteLine("Grade :" + Grade);
+            Console.WriteLine("Marks : " + Marks);   
+            Console.WriteLine("Fees : " + Fees);
+        }
+    }
+    public class Staff:Inheritance
+    {
+        public double Salary;
+        public string Designation;
+
+        public void StaffMethod()
+        {
+            Console.WriteLine("Salary of staff:" + Salary);
+            Console.WriteLine("Designation :" +Designation);
+        }
+    }
+    public class Teaching:Staff
+    {
+        public string Qualification,Subject;
+        public void TeachingMethod()
+        {
+            Console.WriteLine("Qualification of teacher :" + Qualification);
+            Console.WriteLine("Subject of teacher :" + Subject);
+        }
+    }
+    
+}
--- OOPSConceptProblems/OOPSConceptProblems/Program.cs
+++ OOPSConceptProblems/OOPSConceptProblems/Program.cs
@@ -2,26 +2,63 @@
 
 namespace OOPSConceptProblem
 {
-    public class Program
+    public class Program : Inheritance
     {
         public static void Main(string[] args)
         {
-           /* Methods obj= new Methods();
-            obj.Parameterizedmethod("sara",24);
-            obj.Addmethod(2,5);
-            obj.Nonparameterized();*/
+            /* Methods obj= new Methods();
+             obj.Parameterizedmethod("sara",24);
+             obj.Addmethod(2,5);
+             obj.Nonparameterized();*/
 
 
-            ClassandObject name = new ClassandObject();
-            name.TVname = "LG";
-            name.cost = 98000;
-            name.classandObject();
+            /* ClassandObject name = new ClassandObject();
+             name.TVname = "LG";
+             name.cost = 98000;
+             name.classandObject();
 
-            ClassandObject name_one = new ClassandObject();
-            name_one.TVname = "Sony";
-            name_one.cost = 67000;
-            name_one.classandObject();
+             ClassandObject name_one = new ClassandObject();
+             name_one.TVname = "Sony";
+             name_one.cost = 67000;
+             name_one.classandObject();*/
 
+            Student obj =new Student();
+            obj.Id = 1;
+            obj.Name = "sai";
+            obj.Address = "Hyd";
+            obj.Phone = "9876545785";
+            obj.Method();
+             obj.Class = 1;
+            obj.Grade = 'A';
+            obj.Marks = 554;
+             obj.Fees = 22000;
+            obj.ClassMethod();
+            Console.ReadLine();
+
+            Staff obj_one=new Staff();
+            obj_one.Salary = 23000;
+            obj_one.Designation = "MathTeacher";
+            obj_one.StaffMethod();
+
+            Teaching name = new Teaching();
+            name.Id = 2;
+            name.Name = "pari";
+            name.Address = "AP";
+            name.Phone = "9865456658";
+            name.Method();
+            name.Salary = 12000;
+            name.Designation = "MT";
+            name.StaffMethod();
+            name.Subject = "Maths";
+            name.Qualification = "Ms";
+            name.TeachingMethod();
+
+
+
+
+           /* ChildClass obj_one=new ChildClass();
+            obj_one.ChildMethod();
+           // obj_one.ParentMethod();*/
 
         }
     }
