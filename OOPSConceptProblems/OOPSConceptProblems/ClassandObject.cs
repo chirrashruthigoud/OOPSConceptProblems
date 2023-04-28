d6372e7b9c12ab237684de454453f665e8eb27c5
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-04-28 17:30:27 +0530

    Add : class and object

---
+++ OOPSConceptProblems/OOPSConceptProblems/ClassandObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConceptProblems
+{
+    public class ClassandObject
+    {
+
+        public string TVname;
+        public int cost;
+        public void classandObject() 
+        {
+            Console.WriteLine("Say Hello! "  + TVname + " TV cost is :" +cost);
+
+        }
+    }
+}
--- OOPSConceptProblems/OOPSConceptProblems/Program.cs
+++ OOPSConceptProblems/OOPSConceptProblems/Program.cs
@@ -6,10 +6,22 @@ namespace OOPSConceptProblem
     {
         public static void Main(string[] args)
         {
-            Methods obj= new Methods();
+           /* Methods obj= new Methods();
             obj.Parameterizedmethod("sara",24);
             obj.Addmethod(2,5);
-            obj.Nonparameterized();
+            obj.Nonparameterized();*/
+
+
+            ClassandObject name = new ClassandObject();
+            name.TVname = "LG";
+            name.cost = 98000;
+            name.classandObject();
+
+            ClassandObject name_one = new ClassandObject();
+            name_one.TVname = "Sony";
+            name_one.cost = 67000;
+            name_one.classandObject();
+
 
         }
     }
