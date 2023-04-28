a13abe69b524a6573b52e88d5f8636a32d37da50
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-04-28 17:07:37 +0530

    Add : creating the method type

---
+++ OOPSConceptProblems/OOPSConceptProblems/Methods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConceptProblems
+{
+    public class Methods
+    {
+        
+        public void Parameterizedmethod(String name, int age)
+        {
+            string add = name + age;
+
+            Console.WriteLine(name + "," + age);
+        }
+        public void Addmethod(int a, int b)
+        {
+            int add = a + b;
+            int mult = a - b;
+            Console.WriteLine("Number :" + add );
+            Console.WriteLine("Number :" +mult);
+        }
+        public void Nonparameterized()
+        {
+            int a = 60; int b = 6;
+            int div = a/b;
+            Console.WriteLine(div);
+            Console.WriteLine("shruthi");
+
+        }
+
+    }
+}
---
+++ OOPSConceptProblems/OOPSConceptProblems/Program.cs
@@ -0,0 +1,16 @@
+﻿using OOPSConceptProblems;
+
+namespace OOPSConceptProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Methods obj= new Methods();
+            obj.Parameterizedmethod("sara",24);
+            obj.Addmethod(2,5);
+            obj.Nonparameterized();
+
+        }
+    }
+}
